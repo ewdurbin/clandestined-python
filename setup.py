a1ea49587c25ada0e8576203efa3a6aa513ab85a
Ernest W. Durbin III <ewdurbin@gmail.com>
2014-07-06 15:14:58 -0400

    work around http://bugs.python.org/issue21121

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 from distutils.core import setup
 from distutils.core import Extension
 
+extra_compile_args = ["-Wno-error=declaration-after-statement"]
+
 requirements = []
 test_requirements = []
 
@@ -13,7 +15,8 @@ setup(
     description='rendezvous hashing implementation based on murmur3 hash',
     url='https://github.com/ewdurbin/clandestiny',
     packages=['clandestine'],
-    ext_modules=[Extension('clandestine._murmur3', ['ext/_murmur3.c'])],
+    ext_modules=[Extension('clandestine._murmur3', ['ext/_murmur3.c'],
+                           extra_compile_args=extra_compile_args)],
     scripts=[],
     test_suite='tests',
     install_requires=requirements,

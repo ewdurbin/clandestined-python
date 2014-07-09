17378c52179b1872843f28b0a4d44d8142d2c1e5
Ernest W. Durbin III <ewdurbin@gmail.com>
2014-07-09 09:30:54 -0400

    restructure collision test, wrap some tests for string coersion of node_ids

--- clandestined/test/test_collision.py
+++ clandestined/test/test_collision.py
@@ -2,6 +2,8 @@ import sys
 import unittest
 
 import clandestined
+from clandestined import RendezvousHash
+from clandestined import Cluster
 
 def mock_murmur3_32(key, seed=0):
     return 4294967295
@@ -15,24 +17,34 @@ class CollisionTestCase(unittest.TestCase):
     def tearDown(self):
         clandestined._murmur3.murmur3_32 = self.original_murmur3_32
 
-    def test_rendezvous_collision(self):
-        from clandestined import RendezvousHash
-        nodes = ['c', 'b', 'a']
-        rendezvous = RendezvousHash(nodes)
+    def test_monkey_patch(self):
+        rendezvous = RendezvousHash()
         self.assertEqual(rendezvous.hash_function('lol'), 4294967295)
         self.assertEqual(rendezvous.hash_function('wat'), 4294967295)
+
+    def test_rendezvous_collision(self):
+        nodes = ['c', 'b', 'a']
+        rendezvous = RendezvousHash(nodes)
         for i in range(1000):
             self.assertEqual('c', rendezvous.find_node(i))
 
+    def test_rendezvous_names(self):
+        nodes = [1, 2, 3, 'a', 'b', 'lol.wat.com']
+        rendezvous = RendezvousHash(nodes)
+        for i in range(10):
+            self.assertEqual('lol.wat.com', rendezvous.find_node(i))
+        nodes = [1, 'a', '0']
+        rendezvous = RendezvousHash(nodes)
+        for i in range(10):
+            self.assertEqual('a', rendezvous.find_node(i))
+
     def test_cluster_collision(self):
-        from clandestined import Cluster
         nodes = {'1': {'zone': 'a'}, '2': {'zone': 'a'},
                  '3': {'zone': 'b'}, '4': {'zone': 'b'}}
         cluster = Cluster(nodes)
         for n in range(100):
             for m in range(100):
                 self.assertEqual(['2', '4'], sorted(cluster.find_nodes_by_index(n, m)))
-
 
 if __name__ == "__main__":
     unittest.main()
